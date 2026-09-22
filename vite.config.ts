a75b67b703243a1444d64ba7a0f83e64ed1e9ef4
GabriehlV02 <160073087+GabriehlV02@users.noreply.github.com>
2026-09-22 16:51:04 -0400

    feat: add branding and configure port 6060

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], server: { port: 5070, strictPort: false } });
+export default defineConfig({ plugins: [react()], server: { port: 6060, strictPort: false } });
